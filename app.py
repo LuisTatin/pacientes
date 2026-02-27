b770d68370fb71cceb480e23c4fa0fe8dceb743b
LuisTatin <141836556+LuisTatin@users.noreply.github.com>
2026-02-27 18:57:26 -0300

    V8 - imagem e apis + Banco de dados

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ import os
 app = Flask(__name__)
 
 #Configuração de Pastas de Arquivos
-UPLOAD_FOLDER = "uploads"
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
 
 # Configuração do banco de dados SQLite persistente
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
